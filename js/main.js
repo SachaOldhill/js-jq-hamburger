51abfa93bb2105a1b354b514d37528b0b623ec8f
Sacha <sachacollev@gmail.com>
2021-03-18 16:18:55 +0100

    fine es

--- js/main.js
+++ js/main.js
@@ -2,3 +2,7 @@ $('.fa-bars').click(function(){
   $('.hamburger-menu').addClass('active');
   console.log('click');
 });
+$('.fa-times').click(function(){
+  $('.hamburger-menu').removeClass('active');
+  console.log('click');
+});
